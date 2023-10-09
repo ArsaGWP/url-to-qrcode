87b50e26772a58ab3c2848c6e427b4d5fbda4881
ArsaGWP <arsaaiabsakul@gmail.com>
2023-10-09 22:04:29 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import qrcode
+
+while True:
+
+    url = input('Enter URL: ')
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    data = url
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color='black', back_color='white')
+    img.save('qr.png')
+    img.show()
+
+    print('Sucess >> ' + url)
